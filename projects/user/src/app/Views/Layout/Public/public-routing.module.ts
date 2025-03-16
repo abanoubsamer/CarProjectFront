76d64bbf27a715dd044ee9db6c918e15ca5f404a
BiboSamer <bibosamer12@gmail.com>
2025-03-16 23:07:17 +0200

    AddComponentes

--- projects/user/src/app/Views/Layout/Public/public-routing.module.ts
+++ projects/user/src/app/Views/Layout/Public/public-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'Home',
+        loadComponent: () =>
+          import('./Pages/home/home.component').then((m) => m.HomeComponent),
+      },
+
+      { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PublicRoutingModule { }
+export class PublicRoutingModule {}
