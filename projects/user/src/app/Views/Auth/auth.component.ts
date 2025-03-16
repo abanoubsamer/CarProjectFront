ecf7c425747b042e4466c61cf4d852a7e9d29c52
BiboSamer <bibosamer12@gmail.com>
2025-03-16 23:29:37 +0200

    AddStyle

--- projects/user/src/app/Views/Auth/auth.component.ts
+++ projects/user/src/app/Views/Auth/auth.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
