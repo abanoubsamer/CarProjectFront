fdf72f3da1bd055ac2cb730071b9029729c2f4e5
sarakhairy10 <skhairy127@gmail.com>
2025-04-22 21:05:54 +0200

    edit cart and product

--- projects/user/src/app/Views/Layout/Public/Pages/product-detials/product-detials.component.ts
+++ projects/user/src/app/Views/Layout/Public/Pages/product-detials/product-detials.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
 @Component({
   selector: 'app-product-detials',
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './product-detials.component.html',
-  styleUrl: './product-detials.component.css',
+  styleUrl: './product-detials.component.css'
 })
-export class ProductDetialsComponent {
-  
-}
+export class ProductDetialsComponent  {
+
+  }
+
+
+
+
+
+
--- projects/user/src/app/Views/Layout/security/cart/cart.component.ts
+++ projects/user/src/app/Views/Layout/security/cart/cart.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../../../Services/Cart/cart.service';
+
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent  {
 
 }
