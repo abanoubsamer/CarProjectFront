d5793308e6cf9a5b46c20568e0e2c320a1e11a82
sarakhairy10 <skhairy127@gmail.com>
2025-04-23 00:26:26 +0200

    product

--- projects/user/src/app/Views/Layout/Public/Pages/product-detials/product-detials.component.ts
+++ projects/user/src/app/Views/Layout/Public/Pages/product-detials/product-detials.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { GetProductById } from './../../../../../Services/Product/Queries/Models/GetProductById';
+import { Component,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductRatingComponent } from './product-rating/product-rating.component';
 import { CustomerReviewsComponent } from './customer-reviews/customer-reviews.component';
+
 
 @Component({
   selector: 'app-product-detials',
@@ -9,4 +11,13 @@ import { CustomerReviewsComponent } from './customer-reviews/customer-reviews.co
   templateUrl: './product-detials.component.html',
   styleUrl: './product-detials.component.css',
 })
-export class ProductDetialsComponent {}
+export class ProductDetialsComponent  {
+  product: GetProductById=
+  { }as GetProductById
+  // private readonly _productQuereisService = injectt(ProductQuereisService)
+  GetProductById ( productid:string){
+
+  }
+
+
+}
