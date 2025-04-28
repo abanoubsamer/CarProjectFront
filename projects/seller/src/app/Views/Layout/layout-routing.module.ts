76e58602ae4dcabdf6127a3adcbc37f718b54d49
BiboSamer <bibosamer12@gmail.com>
2025-04-28 22:18:37 +0300

    Add Routing And Structuer Project

---
+++ projects/seller/src/app/Views/Layout/layout-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'Dashboard',
+        loadComponent: () =>
+          import('./Pages/dashboard/dashboard.component').then(
+            (c) => c.DashboardComponent
+          ),
+      },
+
+      { path: '', redirectTo: 'Dashboard', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LayoutRoutingModule {}
