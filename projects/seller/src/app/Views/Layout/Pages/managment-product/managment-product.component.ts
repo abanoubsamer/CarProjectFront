e2235d803ac6025013664de062ef50d55e15879b
adhamahmed414 <adhamsokar678@gmail.com>
2025-05-10 18:32:08 +0300

    Create components For Managment Product and Edit Product

---
+++ projects/seller/src/app/Views/Layout/Pages/managment-product/managment-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managment-product',
+  imports: [],
+  templateUrl: './managment-product.component.html',
+  styleUrl: './managment-product.component.css'
+})
+export class ManagmentProductComponent {
+
+}
