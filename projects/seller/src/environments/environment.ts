688f18ac281dc0eaad3a8b76f4cfd74719dde400
unknown <bibosamer12@gmail.comn>
2025-06-18 04:03:02 +0300

    addenviromint

---
+++ projects/seller/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  Ip: 'https://localhost:7154',
+  firebase: {
+    apiKey: 'AIzaSyAtxBlXbktsYinFT-ubAAHdH8pX0xotzHU',
+    authDomain: 'chat-3afa0.firebaseapp.com',
+    projectId: 'chat-3afa0',
+    storageBucket: 'chat-3afa0.firebasestorage.app',
+    messagingSenderId: '213036366957',
+    appId: '1:213036366957:web:7163e1647998532cd11364',
+    measurementId: 'G-GM49HM2W8V',
+    vapidKey:
+      'BD5RLll_hzgsuIMp1ksRmFKQwsX1JlEeMCe8B0en-zd5kEyIV-i8IKEg-qZt563Cg6sOwPb-s-nWLtrxvRKgK7M',
+  },
+};
